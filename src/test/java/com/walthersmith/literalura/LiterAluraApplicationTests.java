aaf8ac2567382881d744a02e2ded7dbe355b3ed5
walthersmith <wsfrancootero@gmail.com>
2025-01-06 19:44:14 -0500

    first commit

---
+++ src/test/java/com/walthersmith/literalura/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walthersmith.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
